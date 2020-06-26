2423781ff7993679a7142b71841c89512d9d07b2
Ahmed Gamal <agmghazi@hotmail.com>
2020-06-27 02:12:11 +0300

    Add project files.

---
+++ EmployeeManagement.Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Department
+    {
+        public int DepartmentId { set; get; }
+        public string DepartmentName { set; get; }
+
+    }
+}
